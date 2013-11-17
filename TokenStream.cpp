ae4307923d4080530e0b42b5fdbe2767ed014c68
lolportal-NTC <fpalta90@gmail.com>
2013-11-17 21:07:11 +0400

    warnings are now /Wall first attempt to implement getToken() NOTE: std_lib_facilities was fixed up a bit

--- TokenStream.cpp
+++ TokenStream.cpp
@@ -81,6 +81,44 @@ tValType getPrimary(){
 }
 
 Token getToken(){
-	Token t(0,0);
-	return t;
+	char c;
+	cin >> c;
+	if (!cin)
+		error("Unable to get token");
+	
+	switch (c){
+	case SUM:
+	case SUB:
+	case MUL:
+	case DIV:
+	case MODULO:
+	case OPEN_BRACE:
+	case CLOSE_BRACE:
+		{
+			return Token(c);
+			break;
+		}
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '.':
+		{
+			cin.putback(c);
+			tValType val;
+			cin >> val;
+			if (!cin)
+				error("Unable to get value");
+			return Token(NUMBER,val);
+			break;
+		}
+	default:
+		error("Unexpected symbol");
+		break;
+	}
 }
